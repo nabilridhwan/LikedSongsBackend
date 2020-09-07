9a89e7d9a71a2d291501870e43b562c17e97aa99
Nabil Ridhwan <nabridhwan@gmail.com>
2020-09-07 08:52:30 +0800

    Added search suppor for songs + users

--- static/js/main.js
+++ static/js/main.js
@@ -12,10 +12,33 @@ mainApp.controller('ProfileController', ($scope, $http) => {
 })
 
 mainApp.controller('SearchController', ($scope, $http) => {
+
+    // All follows following format
+    /*
+        tagline: <user_country|song_artist>
+        image: image of album or profile picture
+        name: <track_name|display_name>
+        type: <song|user>
+    */
     $http.get(`/api/getProfiles`).then((res) => {
         $scope.profiles = res.data.data;
-
         $scope.allLikedSongs = $scope.profiles.map(profile => profile.liked_songs)[0]
-        console.log($scope)
+        $scope.allLikedSongsFormatted = $scope.allLikedSongs.map(song => {
+            return{
+                image: song.track.album.images[0].url,
+                type: "Track",
+                name: song.track.name,
+                tagline: song.track.artists[0].name
+            }
+        })
+        $scope.allProfilesFormatted = $scope.profiles.map(profile => {
+            return {
+                image: profile.image,
+                type: "User",
+                name: profile.name,
+                tagline: profile.spotifyCountry
+            }
+        })
+        $scope.searchData = $scope.allLikedSongsFormatted.concat($scope.allProfilesFormatted)
     })
 })
